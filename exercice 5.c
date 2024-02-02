3b520b8aeb7b20c5175d090cdddf5456d643f1c9
eristanos <64324173+eristanos@users.noreply.github.com>
2024-02-02 20:48:10 +0000

    debut

---
+++ exercice 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n = rand() % 10000 + 1;
+    int guess = -1;
+    while (guess != n)
+    {
+        printf("donnez un nombre : ")        /* code */
+    }
+    
+    
+}
