0ca36844ce751abbd560a73bef0fcc4dcc7c0623
eristanos <64324173+eristanos@users.noreply.github.com>
2024-02-02 21:09:46 +0100

    Update exercice 3.c

---
+++ exercice 3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int n, u;
+	printf("Saisir n et U0: ");
+	scanf("%d %d",&n,&u);
+
+	switch (n)
+	{
+	case 1:
+		u = u + 2 * n;
+		break;
+
+	case 2:
+		u = 5 * n - u;
+		break;
+
+
+	default:
+		u = 2 * n * u;
+		break;
+	}
+	printf("U = %d", u);
+
+}
