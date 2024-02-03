d1e524b6505ae592a499c51fef8489c7a50c325f
eristanos <64324173+eristanos@users.noreply.github.com>
2024-02-03 14:18:31 +0000

    exo 5 fini

--- exercice 5.c
+++ exercice 5.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 int main(){
-    int n = rand() % 10000 + 1;
-    int guess = -1;
+    srand(time(0));
+    int n = rand() %1000 +1;
+    int guess = 2000;
+    printf("%d\n", n);
+    
     while (guess != n)
     {
-        printf("donnez un nombre : ")        /* code */
+        printf("donnez un nombre :\n");
+        scanf("%d", &guess);
+
+        if (guess == n){
+            printf("bravo\n");
+        }
+        else if (guess < n)
+        {
+            printf("trop petit\n");
+        }
+        else if (guess > n){
+            printf("trop grand\n");
+        }
+
     }
+
+    return 0;
     
     
 }
