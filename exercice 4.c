47cf2ae6b35674da654a84bea9d070c404d8dd2f
eristanos <64324173+eristanos@users.noreply.github.com>
2024-02-07 07:45:32 +0000

    on croise les doigts

--- exercice 4.c
+++ exercice 4.c
@@ -4,17 +4,17 @@ int main(){
     float n = 1;
     int nbr_note = 0;
     float somme = 0;
-    float moyenne;
+
     while(n != -1){
         printf("saisir note");
         scanf("%f", &n);
         if (n != -1)
         {
             nbr_note++;
-            somme = somme + n;
+            somme += n;
         }
     }
-    moyenne = somme/nbr_note;
-    printf("%f", moyenne);
+    
+    printf("%f", somme/nbr_note);
     return 0;
 }
