a58027af2ab8f0856602421ec5ae1f62ba2e10dc
eristanos <64324173+eristanos@users.noreply.github.com>
2024-02-07 07:55:46 +0000

    ajustement

--- exercice 5.c
+++ exercice 5.c
@@ -6,32 +6,36 @@
 int main(){
     
     int play = 1;
-    while (play != 0)
+    do
     {
-        srand(time(0));
+        srand(time(NULL));
         int n = rand() %1000 +1;
+        int nbr_tentatives = 0;
         int guess = 2000;
-        while (guess != n)
+        do
         {
             printf("donnez un nombre :\n");
             scanf("%d", &guess);
-
+            nbr_tentatives ++;
             if (guess == n){
-                printf("bravo\n");
+                printf("bravo en %d tentatives seulement !\n", nbr_tentatives);
             }
             else if (guess < n)
             {
                 printf("trop petit\n");
+            
             }
             else if (guess > n){
                 printf("trop grand\n");
+            
             }
 
-        }
+        }while(n != guess);
         printf("veut tu rejouer(0 : non, 1 : oui\n");
         scanf("%d", &play);
         
-    }
+    }while (play == 1);
+    
 
     return 0;
     
