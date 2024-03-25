8df86cf6031ca0d7d71ca82c1c5be2111daea3cd
eristanos <64324173+eristanos@users.noreply.github.com>
2024-03-25 09:13:38 +0100

    finito

--- Caractère/Exercice 4.c
+++ Caractère/Exercice 4.c
@@ -3,12 +3,34 @@
 #include <string.h>
 
 
+
+
+void compteur(char chaine[1000000], int count[256]);
+
 int main() {
     printf("Entrez une chaine de caractères : ");
     char chaine[1000000];
-
-    gets(chaine);
     int count[256];
+    gets(chaine);
+    compteur(chaine, count);
+
+     for(int i = 0; i < 256; i++)
+    {
+        if (count[i] != 0)
+        {
+            printf("%c : %d\n", i, count[i]);
+        }
+    }
+
+    
+    return 0;
+}
+
+
+
+void compteur(char chaine[1000000], int count[256])
+{
+    
 
     for(int i = 0; i < 256; i++)
     {
@@ -17,18 +39,13 @@ int main() {
 
     for(int i = 0; i < strlen(chaine); i++)
     {
-        char lettre = chaine[i];
+        char lettre;
+        if (chaine[i] >= 'A' && chaine[i] <= 'Z')
+        {
+            chaine[i] += 32;
+        }
+        lettre = chaine[i];
         count[lettre]++;
     }
 
-    for(int i = 0; i < 256; i++)
-    {
-        if (count[i] != 0)
-        {
-            printf("%c : %d\n", i, count[i]);
-        }
-    }
-
-
-    return 0;
 }
