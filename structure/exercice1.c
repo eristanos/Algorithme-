bd51eb9ba983d6fc3c0e4cd771c06ec3ab270b8b
eristanos <64324173+eristanos@users.noreply.github.com>
2024-04-02 19:31:52 +0000

    marche mais pas fini je crois

--- structure/exercice1.c
+++ structure/exercice1.c
@@ -5,7 +5,7 @@
 #define NB_CHAR 30
 #define NB_UTILISATEUR 100
 
-void saisi_utilisateur(S_utilisateur tab[], int n);
+
 
 
 struct date 
@@ -27,6 +27,10 @@ struct utilisateur
 
 typedef struct utilisateur S_utilisateur;
 
+void saisi_utilisateur(S_utilisateur tab[], int n);
+
+void affichage_utilisateur(S_utilisateur tab[], int n);
+void ajout_utilisateur(S_utilisateur tab[], int n);
 
 int main()
 {
@@ -35,7 +39,30 @@ int main()
     int n;
     scanf("%d", &n);
     saisi_utilisateur(tableau1, n);
-    
+    printf("quoicoubebu");
+    affichage_utilisateur(tableau1, n);
+    int choix = 0;
+    do
+    {
+        printf("1. Ajouter un utilisateur\n");
+        printf("2. Afficher les utilisateurs\n");
+        printf("3. Quitter\n");
+        scanf("%d", &choix);
+        switch(choix)
+        {
+            case 1:
+                ajout_utilisateur(tableau1, n);
+                n++;
+                break;
+            case 2:
+                affichage_utilisateur(tableau1, n);
+                break;
+            case 3:
+                break;
+            default:
+                printf("Choix invalide\n");
+        }
+    } while(choix != 3);
     return 0;
 }
 
@@ -62,5 +89,57 @@ void saisi_utilisateur(S_utilisateur tab[], int n)
         printf("annee : ");
         scanf("%d", &tab[i].date_creation.annee);
         printf("\n");
+
     }
+}
+
+
+void affichage_utilisateur(S_utilisateur tab[], int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        printf("utilisateur %d\n", i+1);
+        printf("login : %s\n", tab[i].login);
+        printf("password : %s\n", tab[i].password);
+        printf("entite : %s\n", tab[i].entite);
+        printf("date de creation : %d/%d/%d\n", tab[i].date_creation.jour, tab[i].date_creation.mois, tab[i].date_creation.annee);
+
+    }
+}   
+
+void ajout_utilisateur(S_utilisateur tab[], int n)
+{
+    printf("login : ");
+    char phrase[NB_CHAR];
+    scanf("%s", phrase);
+    int nb = 0;
+    n++;
+
+    while(strcmp(tab[nb].login, phrase) < 0 && nb < n)
+    {
+        nb++;
+    }
+    printf("nb : %d\n", nb);
+    for(int i = n; i > nb; i--)
+    {
+        tab[i] = tab[i-1];
+    }
+    strcpy(tab[nb].login, phrase);
+    printf("\n");
+    printf("password : ");
+    scanf("%s", tab[nb].password);
+    printf("\n");
+    printf("entite : ");
+    scanf("%s", tab[nb].entite);
+    printf("\n");
+    printf("date de creation : ");
+    printf("jour : ");
+    scanf("%d", &tab[nb].date_creation.jour);
+    printf("\n");
+    printf("mois : ");
+    scanf("%d", &tab[nb].date_creation.mois);
+    printf("\n");
+    printf("annee : ");
+    scanf("%d", &tab[nb].date_creation.annee);
+    printf("\n");
 }
