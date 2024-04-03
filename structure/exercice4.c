97776a0e2be041e82d3c530ef7270f4f153b3acd
eristanos <64324173+eristanos@users.noreply.github.com>
2024-04-03 15:10:16 +0000

    tkt

--- structure/exercice4.c
+++ structure/exercice4.c
@@ -4,21 +4,60 @@
 
 #define NB_CAR 3
 
+typedef struct date{
+    int jour;
+    int mois;
+    int annee;
+} S_date;
+
+
+typedef struct entretien{
+    S_date date;
+    char type[30];
+    float temps; //en heure
+    float cout;
+    char mecanicien[30];
+
+
+} S_entretien;
+
+
 typedef struct car {
     char marque[30];
     char modele[30];
-    int immatriculation;
+    char immatriculation[30];
     int annee;
     int km;
     float moyenne_km;
+    S_entretien entretien[100];
+    int nb_entretien;
+
 } S_car;
 
 void Saisir_Car(S_car tab[], int n);
+
+void Saisir_Entretien(S_entretien tab[]);
 
 
 int main() {
     S_car tab[NB_CAR];
     Saisir_Car(tab,3);
+    int etat = 1;
+    while (etat == 1)
+    {
+        printf("voulez vous saisir un entretien ? n pour oui 0 pour non\n");
+        scanf("%d", &etat);
+        if (etat == 1)
+        {
+            Saisir_Entretien(tab[0].entretien);
+            tab[0].nb_entretien++;
+        }
+        else
+        {
+            etat = 0
+        }
+    }
+    
     return 0;
 }
 
@@ -31,7 +70,7 @@ void Saisir_Car(S_car tab[], int n)
         printf("Saisir le modele de la voiture %d\n", i + 1);
         scanf("%s", tab[i].modele);
         printf("Saisir l'immatriculation de la voiture %d\n", i + 1);
-        scanf("%d", &tab[i].immatriculation);
+        scanf("%s", tab[i].immatriculation);
         printf("Saisir l'annee de la voiture %d\n", i + 1);
         scanf("%d", &tab[i].annee);
         printf("Saisir le kilometrage de la voiture sur 5 ans%d\n", i + 1);
@@ -40,5 +79,26 @@ void Saisir_Car(S_car tab[], int n)
         scanf("%f", &tab[i].moyenne_km);
     }
 }
+
+void Saisir_Entretien(S_entretien tab[])
+{
+    printf("Saisir la date de l'entretien\n");
+    printf("Saisir le jour\n");
+    scanf("%d", &tab->date.jour);
+    printf("Saisir le mois\n");
+    scanf("%d", &tab->date.mois);
+    printf("Saisir l'annee\n");
+    scanf("%d", &tab->date.annee);
+    printf("Saisir le type de l'entretien\n");
+    scanf("%s", tab->type);
+    printf("Saisir le temps de l'entretien\n");
+    scanf("%f", &tab->temps);
+    printf("Saisir le cout de l'entretien\n");
+    scanf("%f", &tab->cout);
+    printf("Saisir le mecanicien de l'entretien\n");
+    scanf("%s", tab->mecanicien);
+}
+
+
 
 
